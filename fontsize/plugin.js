e5208b87dc846ab3d3fc7fee6aee2d5313a364f0
Filipe Tomás <filipe.m.a.tomas@gmail.com>
2018-01-31 21:48:42 +0000

    first commit. Added plugin and icons

---
+++ fontsize/plugin.js
@@ -0,0 +1,73 @@
+﻿(function() {
+
+	CKEDITOR.plugins.add( 'fontsize', {
+		icons: 'fontincrease,fontdecrease',
+		hidpi: true,
+		init: function( editor ) {
+			var config = editor.config;
+
+			addCommand(editor, 'increaseFontSize', increaseFontSize);
+			addCommand(editor, 'decreaseFontSize', decreaseFontSize);
+			
+			addButton( editor, 'FontIncrease', 'Increase font size' , 'increaseFontSize');
+			addButton( editor, 'FontDecrease', 'Decrease font size' , 'decreaseFontSize');
+		}
+	} );
+	
+	function addButton( editor, name, title, command ) 
+	{
+		editor.ui.addButton(name, 
+		{ 
+			label: title,
+			command: command,
+			toolbar: 'styles'
+		});
+	}
+	
+	function addCommand(editor, name, func)
+	{
+		editor.addCommand(name, {
+			exec : function(editor) {				
+				func(editor);
+			}
+		});
+	}
+	
+	function increaseFontSize(editor)
+	{
+		changeFontSize(editor, 1);
+	}
+	
+	function decreaseFontSize(editor)
+	{
+		changeFontSize(editor, -1);
+	}
+	
+	function changeFontSize(editor, plus)
+	{			
+		var span = editor.elementPath().contains( function( element ) {        
+			return element.is( 'span' ) && element.getStyle( 'font-size' );
+		}); 
+		
+		var fontSize = span ? span.getStyle( 'font-size' ) : "";
+
+		if(!fontSize) { fontSize = editor.config.fontSize_default; }
+		
+		var value = parseInt(fontSize, 10) + plus;
+		
+		if(value <= 0 ) { value = 1; }
+		
+		var style = new CKEDITOR.style({
+			element: 'span',
+			attributes: {
+				'style': 'font-size:' + value + 'px'
+			}
+		});
+		
+		editor.applyStyle(style);
+	}
+	
+	
+} )();
+
+CKEDITOR.config.fontSize_default = "15px";
